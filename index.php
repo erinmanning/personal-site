a9879fb170d8a2c7dda21a3c81b89f826f51d888
erinmanning <erin@erinmanning.net>
2013-10-10 13:46:50 -0400

    copy files to gh pages

---
+++ index.php
@@ -0,0 +1,97 @@
+<!doctype html>
+
+<!--[if lt IE 7 ]> <html class="ie ie6 no-js" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie ie7 no-js" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 no-js" lang="en"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie ie9 no-js" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<!-- the "no-js" class is for Modernizr. -->
+
+<head id="www-erinmanning-net" data-template-set="html5-reset">
+
+	<meta charset="utf-8">
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	
+	<title>Erin Manning | Dayton, OH Graphic Design</title>
+	
+	<meta name="title" content="Erin Manning | Dayton, OH Graphic Design">
+	<meta name="description" content="Website of Erin Manning, Graphic Designer in Dayton, Ohio.">
+	
+	<meta name="author" content="Erin Manning">
+	<meta name="copyright" content="Copyright Erin Manning . All Rights Reserved.">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=1">
+	 
+
+	<link rel="shortcut icon" href="_/img/favicon.ico">
+
+	<link rel="stylesheet" href="_/css/style.css">
+	<!--[if IE]><link rel="stylesheet" type="text/css" href="_/css/ie.css"><![endif]-->
+
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,800,400italic' rel='stylesheet' type='text/css'>
+	
+	<!-- all our JS is at the bottom of the page, except for Modernizr. -->
+	<script src="_/js/modernizr-1.7.min.js"></script>
+
+</head>
+
+<body>
+
+<div class="wrapper">
+
+	<header role="banner">
+		<h1><a class="bio-toggle" href="#" title="Continue reading about Erin">Erin Manning</a></h1>
+	</header>
+		
+		<section id="about">
+			<div class="bio hidden group">
+				<img class="headshot" src="_/img/headshot.jpg" alt="Erin Manning" title="Erin Manning" />
+				<p>&hellip; is a designer living in Dayton (Midwest Ohio for y'all out-of-towners), who loves to think and create.</p>
+				<p>She has an obsession with learning, mostly about design and web development, as well as a lot of other nerdy subjects. For the curious few, you could also describe her as an eco-minded, bread and coffee loving, crochet rockin&#146; mommy with a penchant for <em>good</em> science fiction.</p>
+				<p>Interested in collaboration? Send a <a href="mailto:info@erinmanning.net?Subject=Why%20Hello%20There">quick message</a> with the details.</p>
+			</div>
+		</section>
+	
+	<nav id="socialmedia">
+		<ul class="socialmedia group">				
+			<li><a class="work" href="http://www.behance.net/ErinManning" title="Check out my work on Behance">Works</a></li>
+			<!--<li><a class="blog" href="http://scriptogr.am/erinmanning" title="Read my Tumblr">Blogs</a>-->
+			<li><a class="twitter" href="http://www.twitter.com/_erinmanning" title="Follow me on Twitter">Tweets</a></li>
+			<li><a class="books" href="http://www.goodreads.com/user/show/6088668-erin-manning" title="See my bookshelf">Reads</a></li>
+			<li><a class="music" href="http://www.last.fm/user/ejmanning84" title="See my latest listens on Last.fm">Rocks</a></li>
+		</ul>
+	</nav>	
+
+	<footer>
+		<p>&copy; <?php print date("Y");?> Erin Manning // <a href="mailto:info@erinmanning.net">Contact</a></p>		
+	</footer>
+
+</div><!-- /wrapper -->
+
+<!-- javascript -->
+
+<!-- Grab Google CDN's jQuery. fall back to local if necessary -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+<script>window.jQuery || document.write("<script src='_/js/jquery-1.5.1.min.js'>\x3C/script>")</script>
+
+<script src="_/js/functions.js"></script>
+	 
+<script>
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-21253745-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+  
+</body>
+</html>
